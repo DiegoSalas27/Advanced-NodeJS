d663dffa123edb494defd48f6d007607f6d64727
DiegoSalas27 <dominicsc2hs@gmail.com>
2022-08-16 16:46:57 -0400

    test: ensure Service calls LoadFacebookUserApi once

--- src/data/services/facebook-authentication.spec.ts
+++ src/data/services/facebook-authentication.spec.ts
@@ -4,10 +4,12 @@ import { FacebookAuthenticationService } from './facebook-authentication'
 
 class LoadFacebookUserApiSpy implements LoadFacebookUserApi {
   token?: string
+  callsCount = 0
   result = undefined
 
   async loadUser (params: LoadFacebookUserApi.Params): Promise<LoadFacebookUserApi.Result> {
     this.token = params.token
+    this.callsCount++
     return this.result
   }
 }
@@ -21,6 +23,7 @@ describe('FacebookAuthenticationService', () => {
     await sut.perform({ token: 'any_token' })
 
     expect(loadFacebookUserApiSpy.token).toBe('any_token')
+    expect(loadFacebookUserApiSpy.callsCount).toBe(1)
   })
 
   test('Should return AuthenticationError when LoadFacebookUserApi returns undefined', async () => {
