ed19446022b508c90f8d48c175773f26260e2717
DiegoSalas27 <dominicsc2hs@gmail.com>
2022-08-17 17:35:32 -0400

    test: ensure FacebookApi gets app token with correct params

--- src/infrastructure/api/facebook.spec.ts
+++ src/infrastructure/api/facebook.spec.ts
@@ -2,15 +2,23 @@ import { mock } from 'jest-mock-extended'
 import { HttpGetClient, FacebookApi } from './facebook'
 
 describe('FacebookApi', () => {
-  test('Should get app token', async () => {
+  const clientId = 'any_client_id'
+  const clientSecret = 'any_client_secret'
+
+  test('Should get app token with correct params', async () => {
     const httpClient = mock<HttpGetClient>()
 
-    const sut = new FacebookApi(httpClient)
+    const sut = new FacebookApi(httpClient, clientId, clientSecret)
 
     await sut.loadUser({ token: 'any_client_token' })
 
     expect(httpClient.get).toHaveBeenCalledWith({
-      url: 'https://graph.facebook.com/oauth/access_token'
+      url: 'https://graph.facebook.com/oauth/access_token',
+      params: {
+        client_id: clientId,
+        client_secret: clientSecret,
+        grant_type: 'client_credentials'
+      }
     })
   })
 })
