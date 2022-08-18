639201b0a01737a11b37419a6e479a71e187ff22
DiegoSalas27 <dominicsc2hs@gmail.com>
2022-08-17 23:16:06 -0400

    test: ensure JwtTokenGenerator returns a token on success

--- src/infrastructure/crypto/jwt-token-generator.spec.ts
+++ src/infrastructure/crypto/jwt-token-generator.spec.ts
@@ -8,6 +8,7 @@ describe('JwtTokenGenerator', () => {
   let fakeJwt: jest.Mocked<typeof jwt>
   beforeAll(() => {
     fakeJwt = jwt as jest.Mocked<typeof jwt>
+    fakeJwt.sign.mockImplementation(() => 'any_token')
   })
 
   beforeEach(() => {
@@ -18,5 +19,11 @@ describe('JwtTokenGenerator', () => {
     await sut.generateToken({ key: 'any_key', expirationInMs: 1000 })
 
     expect(fakeJwt.sign).toHaveBeenCalledWith({ key: 'any_key' }, 'any_secret', { expiresIn: 1 })
+  })
+
+  test('Should return a token on success', async () => {
+    const token = await sut.generateToken({ key: 'any_key', expirationInMs: 1000 })
+
+    expect(token).toBe('any_token')
   })
 })
