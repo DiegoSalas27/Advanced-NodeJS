2f5e6516bfe9cc232a936b3a29b585b499989549
DiegoSalas27 <dominicsc2hs@gmail.com>
2022-08-17 21:59:31 -0400

    test: ensure AxiosHttpClient returns data on success

--- src/infrastructure/http/axios-client.spec.ts
+++ src/infrastructure/http/axios-client.spec.ts
@@ -10,6 +10,10 @@ describe('AxiosHttpClient', () => {
   let params: object
   beforeAll(() => {
     fakeAxios = axios as jest.Mocked<typeof axios>
+    fakeAxios.get.mockResolvedValue({
+      status: 200,
+      data: 'any_data'
+    })
     url = 'any_url'
     params = {
       any: 'any'
@@ -26,6 +30,12 @@ describe('AxiosHttpClient', () => {
 
       expect(fakeAxios.get).toHaveBeenCalledWith(url, { params })
       expect(fakeAxios.get).toHaveBeenCalledTimes(1)
+    })
+
+    test('Should return data on success', async () => {
+      const result = await sut.get({ url, params })
+
+      expect(result).toEqual('any_data')
     })
   })
 })
